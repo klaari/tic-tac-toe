c40918c9cd1c84250a2039ff8a0888986238b35a
Kari <kari.laari@gmail.com>
2018-09-27 17:01:48 +0300

    Init with lifted state up

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={ this.props.onclick }
+            >
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            player: 'X',
+        };
+
+    }
+
+    handleClick(i) {
+
+        const squares = this.state.squares.slice();
+
+        if(squares[i] !== null) return;
+
+        squares[i] = this.state.player;
+
+        let nextPlayer = (this.state.player === 'X') ? 'Y' : 'X';
+
+        this.setState({
+            squares: squares,
+            player: nextPlayer
+        });
+    }
+
+
+    renderSquare(i) {
+        return (
+            <Square
+                onclick={ () => this.handleClick( i ) }
+                value={this.state.squares[i]}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next player: ' + this.state.player;
+
+        return (
+            <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
